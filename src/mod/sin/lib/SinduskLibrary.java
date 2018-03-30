ca8c8001c749750a97cc45dbcf8b08d0358aae4c
Sindusk <SinisteRing@gmail.com>
2018-03-30 18:03:52 -0400

    Initial Commit

---
+++ src/mod/sin/lib/SinduskLibrary.java
@@ -0,0 +1,62 @@
+package mod.sin.lib;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
+import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+
+public class SinduskLibrary
+implements WurmServerMod,
+Configurable,
+PreInitable {
+    boolean bDebug = false;
+    
+    private Logger logger;
+
+    public SinduskLibrary() {
+        this.logger = Logger.getLogger(this.getClass().getName());
+    }
+    
+    public void configure(Properties properties) {
+    	this.bDebug = Boolean.parseBoolean(properties.getProperty("debug", Boolean.toString(this.bDebug)));
+        try {
+            String logsPath = Paths.get("mods", new String[0]) + "/logs/";
+            File newDirectory = new File(logsPath);
+            if (!newDirectory.exists()) {
+                newDirectory.mkdirs();
+            }
+            FileHandler fh = new FileHandler(String.valueOf(String.valueOf(logsPath)) + this.getClass().getSimpleName() + ".log", 10240000, 200, true);
+            if (this.bDebug) {
+                fh.setLevel(Level.INFO);
+            } else {
+                fh.setLevel(Level.WARNING);
+            }
+            fh.setFormatter(new SimpleFormatter());
+            this.logger.addHandler(fh);
+        }
+        catch (IOException ie) {
+            System.err.println(String.valueOf(this.getClass().getName()) + ": Unable to add file handler to logger");
+        }
+        this.Debug("Debugging messages are enabled.");
+    }
+
+    private void Debug(String x) {
+        if (this.bDebug) {
+            System.out.println(String.valueOf(this.getClass().getSimpleName()) + ": " + x);
+            System.out.flush();
+            this.logger.log(Level.INFO, x);
+        }
+    }
+    
+    public void preInit() {
+    }
+
+}
---
+++ src/mod/sin/lib/Util.java
@@ -0,0 +1,130 @@
+package mod.sin.lib;
+
+import java.util.logging.Logger;
+
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.NotFoundException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
+public class Util {
+	public static final Logger logger = Logger.getLogger(Util.class.getName());
+
+	private static boolean success = false;
+	private static String reason = "";
+	public static void setReason(String reason){
+		Util.reason = " [Reason: "+reason+"]";
+	}
+	private static void checkSuccess(int type, Class<?> instrumentingClass, CtClass editClass, String declaredMethod, String methodCall){
+		String editType = "Instrument";
+		if(type == 1){
+			editType = "Insert Before";
+		}else if(type == 2){
+			editType = "Set Body";
+		}
+		String errorType = editType.toUpperCase();
+		Logger classLogger = Logger.getLogger(instrumentingClass.getName());
+		if(success){
+			classLogger.info(editType+": "+editClass.getSimpleName()+" - "+declaredMethod+" "+(methodCall.length() > 0 ? "call to "+methodCall+" " : "")+"successful."+reason);
+		}else{
+			classLogger.severe("["+errorType+" ERROR] from "+instrumentingClass.getSimpleName()+"! Could not "+editType+" "+editClass.getName()+" - "+declaredMethod+(methodCall.length() > 0 ? " call to "+methodCall+" " : "")+"!"+reason);
+		}
+		success = false;
+		reason = "";
+	}
+	public static void instrumentDeclared(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String methodCall, String replace){
+        try {
+			ctToInstrument.getDeclaredMethod(declaredMethod).instrument(new ExprEditor(){
+			    public void edit(MethodCall m) throws CannotCompileException {
+			        if (m.getMethodName().equals(methodCall)) {
+			            m.replace(replace);
+			            success = true;
+			            return;
+			        }
+			    }
+			});
+			checkSuccess(0, instrumentingClass, ctToInstrument, declaredMethod, methodCall);
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(0, instrumentingClass, ctToInstrument, declaredMethod, methodCall);
+		}
+	}
+	public static void instrumentDescribed(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String descriptor, String methodCall, String replace){
+        try {
+			ctToInstrument.getMethod(declaredMethod, descriptor).instrument(new ExprEditor(){
+			    public void edit(MethodCall m) throws CannotCompileException {
+			        if (m.getMethodName().equals(methodCall)) {
+			            m.replace(replace);
+			            success = true;
+			            return;
+			        }
+			    }
+			});
+			checkSuccess(0, instrumentingClass, ctToInstrument, declaredMethod, methodCall);
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(0, instrumentingClass, ctToInstrument, declaredMethod, methodCall);
+		}
+	}
+	public static void insertAfterDeclared(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String insert){
+		try {
+			ctToInstrument.getDeclaredMethod(declaredMethod).insertAfter(insert);
+			success = true;
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+	public static void insertAfterDescribed(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String descriptor, String insert){
+		try {
+			ctToInstrument.getMethod(declaredMethod, descriptor).insertAfter(insert);
+			success = true;
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+	public static void insertBeforeDeclared(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String insert){
+		try {
+			ctToInstrument.getDeclaredMethod(declaredMethod).insertBefore(insert);
+			success = true;
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+	public static void insertBeforeDescribed(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String descriptor, String insert){
+		try {
+			ctToInstrument.getMethod(declaredMethod, descriptor).insertBefore(insert);
+			success = true;
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(1, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+	public static void setBodyDeclared(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String body){
+		try {
+			ctToInstrument.getDeclaredMethod(declaredMethod).setBody(body);
+			success = true;
+			checkSuccess(2, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(2, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+	public static void setBodyDescribed(Class<?> instrumentingClass, CtClass ctToInstrument, String declaredMethod, String descriptor, String body){
+		try {
+			ctToInstrument.getMethod(declaredMethod, descriptor).setBody(body);
+			success = true;
+			checkSuccess(2, instrumentingClass, ctToInstrument, declaredMethod, "");
+		} catch (CannotCompileException | NotFoundException e) {
+			e.printStackTrace();
+			checkSuccess(2, instrumentingClass, ctToInstrument, declaredMethod, "");
+		}
+	}
+}
