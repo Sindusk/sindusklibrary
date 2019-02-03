9717495295bfa41354ed592bec0d7756b3fe1ba9
Sindusk <SinisteRing@gmail.com>
2019-02-03 14:33:21 -0500

    Update ArmourAssist in preparation for Armoury update.

--- src/main/java/mod/sin/lib/ArmourAssist.java
+++ src/main/java/mod/sin/lib/ArmourAssist.java
@@ -1,43 +1,77 @@
 package mod.sin.lib;
 
+import com.wurmonline.server.combat.ArmourTemplate;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class ArmourAssist {
     public static Logger logger = Logger.getLogger(ArmourAssist.class.getName());
 
-    public static HashMap<String, Integer> armourNameToType = new HashMap<>();
-    public static HashMap<Integer, String> armourTypeToName = new HashMap<>();
+    public static ArrayList<ArmourTemplate.ArmourType> armourTypes = new ArrayList<>();
+    public static HashMap<String, Integer> armourNameToByte = new HashMap<>();
+    public static HashMap<Integer, String> armourByteToName = new HashMap<>();
 
-    public static int getArmourType(String str){
-        if(armourNameToType.containsKey(str.toLowerCase())){
-            return armourNameToType.get(str.toLowerCase());
+    public static ArmourTemplate.ArmourType getArmourType(String name){
+        for (ArmourTemplate.ArmourType type : armourTypes){
+            if (type.getName().equals(name)){
+                return type;
+            }
+        }
+        logger.warning(String.format("Armour type %s is not a valid ArmourType in the ArmourAssist system.", name));
+        return null;
+    }
+
+    public static ArmourTemplate.ArmourType getArmourType(int armourType){
+        String name = getArmourName(armourType); // Identify the name of the armour first
+        return getArmourType(name);
+    }
+
+    public static int getArmourByte(String str){
+        if(armourNameToByte.containsKey(str.toLowerCase())){
+            return armourNameToByte.get(str.toLowerCase());
         }
         return Integer.parseInt(str);
     }
 
     public static String getArmourName(int armourType){
-        if(armourTypeToName.containsKey(armourType)){
-            return armourTypeToName.get(armourType);
+        if(armourByteToName.containsKey(armourType)){
+            return armourByteToName.get(armourType);
         }
-        logger.warning(String.format("Armour type %s is unknown to the ArmourAssist system.", armourType));
+        logger.warning(String.format("Armour type %d is unknown to the ArmourAssist system.", armourType));
         return "unknown";
     }
 
     public static void initializeArmourMaps(){
         logger.info("Initializing Armour Maps.");
-        armourNameToType.put("cloth", 6);
-        armourNameToType.put("leather", 1);
-        armourNameToType.put("studded", 2);
-        armourNameToType.put("chain", 3);
-        armourNameToType.put("plate", 4);
-        armourNameToType.put("drake", 9);
-        armourNameToType.put("dragonscale", 10);
-        armourNameToType.put("scale", 7);
-        armourNameToType.put("ring", 5);
-        armourNameToType.put("splint", 8);
-        for(String name : armourNameToType.keySet()){
-            armourTypeToName.put(armourNameToType.get(name), name);
+
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_NONE.getName(), 0);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_CLOTH.getName(), 6);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_LEATHER.getName(), 1);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_STUDDED.getName(), 2);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_CHAIN.getName(), 3);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_PLATE.getName(), 4);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_LEATHER_DRAGON.getName(), 9);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_SCALE_DRAGON.getName(), 10);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_SCALE.getName(), 7);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_RING.getName(), 5);
+        armourNameToByte.put(ArmourTemplate.ARMOUR_TYPE_SPLINT.getName(), 8);
+
+        for(String name : armourNameToByte.keySet()){
+            armourByteToName.put(armourNameToByte.get(name), name);
         }
+
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_NONE);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_CLOTH);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_LEATHER);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_STUDDED);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_CHAIN);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_PLATE);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_LEATHER_DRAGON);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_SCALE_DRAGON);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_SCALE); // Fake scale
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_RING);
+        armourTypes.add(ArmourTemplate.ARMOUR_TYPE_SPLINT);
     }
 }
